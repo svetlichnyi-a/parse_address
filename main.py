856c789b9993be5c01a31413a02a18521c6d89d4
svetlichnyy_av <webtailor@mail.ru>
2024-06-04 09:06:18 +0300

    текущие изменения

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import pandas as pd
 
 print("start")
 
-PATH = r"files_for_parsing/TAP_Diagn_week20240215.csv"  # путь к файлу
+PATH = r"files_for_parsing/TAP_Diagn_week20231221.csv"  # путь к файлу
 COL = "Адрес_пациента_неформализованный"  # колонка которую нужно распарсить
 
-df = pd.read_csv(PATH)  # загружаем таблицу.
+df = pd.read_csv(PATH, encoding="utf-8", sep=",")  # загружаем таблицу.
 
 parsed = parse_address(df, COL, neiro=1, add_mo_norm=1)  # парсим и сохраняем результат в переменную
-parsed.to_excel("PARSED/TAP_Diagn_week20240215_PARSED.xlsx", index=False)  # сохраняем результат в эксель
+parsed.to_excel("PARSED/TAP20231201_or.xlsx", index=False)  # сохраняем результат в эксель
 
 print("end")
